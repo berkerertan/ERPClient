5484d64e4fdb21b3ef39e9119c792606c62b3e42
Berker ERTAN <berkerertan@hotmail.com>
2024-10-16 23:52:32 +0300

    models, pipes, toastr, services added.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { BlankComponent } from '../blank/blank.component';
 import { SectionComponent } from '../section/section.component';
 import { SharedModule } from '../../modules/shared.module';
+import { SwalComponent } from '@sweetalert2/ngx-sweetalert2';
+import { SwalService } from '../../services/swal.service';
 
 @Component({
   selector: 'app-home',
---
+++ src/app/menu.ts
@@ -0,0 +1,39 @@
+export class MenuModel{
+    name: string ="";
+    icon:string ="";
+    url:string="";
+    isTitle:boolean=false;
+    subMenus:MenuModel[]=[];
+}
+
+export const Menus: MenuModel[]=[
+    {
+        name:"Ana Sayfa",
+        icon:"fas fa-solid fa-home",
+        url:"/",
+        isTitle:false,
+        subMenus:[]
+    },
+    // {
+    //     name:"Admin",
+    //     icon:"",
+    //     url:"",
+    //     isTitle:true,
+    //     subMenus:[]
+    // },
+    // {
+    //     name:"Yönetim",
+    //     icon:"far fa fa-users",
+    //     url:"",
+    //     isTitle:false,
+    //     subMenus:[
+    //         {
+    //             name:"Kullanıcılar",
+    //             icon:"far fa-solid fa-user",
+    //             url:"/users",
+    //             isTitle:false,
+    //             subMenus:[]
+    //         }
+    //     ]
+    // }
+]
