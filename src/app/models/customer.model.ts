45b85c2302a594c06967aaad608ec0e094c39731
Berker ERTAN <berkerertan@hotmail.com>
2025-01-20 23:57:55 +0300

    Customer CURD added.

---
+++ src/app/models/customer.model.ts
@@ -0,0 +1,10 @@
+export class CustomerModel{
+    id:string="";
+    name:string="";
+    taxDepartment:string="";
+    taxNumber:string="";
+    city:string="";
+    town:string="";
+    fullAddress:string="";
+
+}
